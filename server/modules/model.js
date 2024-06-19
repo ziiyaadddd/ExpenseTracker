f5fea3c29bfcdfe1b19cc4fc9a8bd2f7a8009d64
ziiyaadddd <104928735+ziiyaadddd@users.noreply.github.com>
2024-06-19 17:54:52 +0530

    Add files via upload

---
+++ server/modules/model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//categories => field => ['type', 'color]
+const categories_model = new Schema({
+    type:{type: String, default:"Investment"},
+    color:{type: String, default:'#FCBE44'}
+})
+
+
+//transactions => field => ['name', 'type', 'amount', 'date']
+
+const transactions_model = new Schema({
+    name:{type: String, default:"Anonymous"},
+    type:{type: String, default: "Investment"},
+    amount:{type: Number},
+    date:{type: Date, default:Date.now}
+})
+
+const Categories = mongoose.model('categories',categories_model)
+const Transaction = mongoose.model('transaction', transactions_model)
+
+exports.default = Transaction;
+module.exports = {
+    Categories,
+    Transaction
+}
+
+
---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config({path:"./config.env"})
+const port = process.env.PORT || 5000;
+
+//use middlewares
+app.use(cors());
+app.use(express.json());
+
+//mongodb connection
+const con = require('./db/connection.js');
+
+//using routes
+app.use(require('./routes/route'));
+
+con.then(db=>{
+    if(!db)return process.exit(1);
+
+    //listen to the server only when we have valid connection to database
+    app.listen(port, ()=>{
+    console.log(`Server is running on port:${port}`)
+    })
+
+    app.on('error',err=>{
+        console.log(`Failed to Connect with HTTP Server:${err}`)
+    });
+
+    //error in mongodb connection
+
+}).catch(error=>{
+    console.log(`Connection Failed...!${error}`);
+})
+
+
